e3c026d253190c9025fa93160859a2483e2b2a99
veresen01 <111127597+veresen01@users.noreply.github.com>
2024-10-11 16:27:12 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,11 +3,3 @@ print('hello world!')
 print('hello again')
 
 print('I like typing this.')
-
-print('This is fun.')
-
-print('Yay! Printing.')
-
-print("I'd much rather you 'not'.")
-
-print('I "said" do not touch this.')
